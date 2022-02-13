749de0ab870c2b166519b445c9fd9889187c5bd4
jon-pruessmeier <pruessmeierjon@gmail.com>
2022-02-13 11:41:24 +0100

    finished the zipping of quotes and users

--- server.js
+++ server.js
@@ -8,9 +8,15 @@ let userArray = [];
 
 let quotes = [];
 
+let usersWithQuotes = [];
+
+function randomItem(items){
+    return items[Math.floor(Math.random()*items.length)]; 
+}
+
 request('https://randomuser.me/api/?results=10', { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
-  console.log(body);
+  //console.log(body);
   const users = body.results.map( user => {
       const newUser = {
           'name': { 'first': user.name.first, 'last' : user.name.last},
@@ -23,9 +29,20 @@ request('https://randomuser.me/api/?results=10', { json: true }, (err, res, body
 
 request('https://type.fit/api/quotes', { json: true }, (err, res, body) => {
   if (err) { return console.log(err); }
-  console.log(body);
+  //console.log(body);
   quotes = body;
 });
+
+setTimeout(() => {
+    console.log(userArray);
+    console.log(quotes);
+    usersWithQuotes = userArray.map( user => {
+        const newQuote = randomItem(quotes).text;
+        user['quote'] = newQuote;
+        return user;
+    });
+    console.log(usersWithQuotes);
+}, 10000);
 
 
 app.get('/persons', (req, res) => {
